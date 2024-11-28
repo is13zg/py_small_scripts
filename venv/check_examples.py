bfda98bbb4b490fc2f0b6b3b47e6d62cc9954c98
is13zg <is13mozg@gmail.com>
2024-11-28 18:22:47 +0300

    Update check_examples.py
    div if possiple

--- venv/check_examples.py
+++ venv/check_examples.py
@@ -1,13 +1,35 @@
 # решение примеров
 
-n = 15
+n = 150
 Answer_Only = 1
-DIV = 1
+DIV = 0
+DIV_if_possible = 1
 Numeration = 1
 Otveti_v_stolbik = 1
 
+
+def safe_eval_decorator(func):
+    def wrapper(expression):
+        try:
+            result = func(expression)
+            if DIV_if_possible:
+                if result == round(result):
+                    return round(result)
+                else:
+                    return result
+        except Exception as e:
+            print(f"Ошибка: {e}")
+            return None
+    return wrapper
+
+eval = safe_eval_decorator(eval)
+
+
 for i in range(1, n + 1):
     s = input()
+    if "." in s:
+        s = s.split(".")[1]
+        s= s.strip()
     s2 = s.replace(' ', "")
     s2 = s2.replace('=', "")
     s2 = s2.replace('х', "*")
@@ -18,6 +40,8 @@ for i in range(1, n + 1):
     s2 = s2.replace('×', "*")
     s2 = s2.replace('—', "-")
     s2 = s2.replace('–', "-")
+    s2 = s2.replace('−', "-")
+    s2 = s2.replace(',', ".")
     if DIV:
         s2 = s2.replace("÷", '//')
         s2 = s2.replace(":", '//')
